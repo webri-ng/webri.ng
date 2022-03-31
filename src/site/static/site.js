4e1833e2850941856b3b97c15178e5d07b0e7346
Anthony <ajxs@panoptic.online>
2022-03-31 23:53:43 +1100

    Front-end initial checkin

--- src/site/static/site.js
+++ src/site/static/site.js
@@ -95,3 +95,98 @@ function createWebring()
 		formErrorMessage.textContent = "An unhandled exception has occurred";
 	});
 }
+
+/**
+ * Creates a random string of a specified length.
+ * @param {CreateRandomStringOptions} [options] - Additional options for the process.
+ * @returns The created random string.
+ */
+function createRandomString()
+{
+	/**
+	 * The ASCII 'Source Characters' array.
+	 * These characters are used to construct the randomised string. The method is
+	 * implemented in this manner since the numbers and letters are not adjacent in the
+	 * ASCII table.
+	 */
+	const sourceCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+		'0123456789_';
+
+	return Array(12).fill(0).map(() => {
+		return sourceCharacters[Math.floor(Math.random() * sourceCharacters.length)];
+	}).join('');
+}
+
+
+function addNewSite()
+{
+	const form = document.getElementById('add-new-site-form');
+	const formErrorMessage = document.getElementById('add-new-site-error-message');
+	const formData = new FormData(form);
+
+	const body = Object.fromEntries(formData.entries());
+	const { webringUrl } = body;
+
+	delete body.webringUrl;
+
+	fetch("/webring/" + webringUrl + "/add", {
+		body: JSON.stringify(body),
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+	}).then((response) => {
+		if (!response.ok) {
+			return response.json().then((errorResponse) => {
+				formErrorMessage.textContent = errorResponse.error;
+			});
+		}
+
+		// Due to the poor redirect implementation, the server simply returns the URL
+		// in a JSON response body, which is then redirected to.
+		return response.json().then((response) => {
+			window.location.href = response.url;
+		});
+	}).catch((err) => {
+		console.error(err);
+		formErrorMessage.textContent = "An unhandled exception has occurred";
+	});
+}
+
+
+
+function insertRandomWebrings()
+{
+	let testDescription = "Nihil repudiandae eum et velit ipsum adipisci harum. " +
+		"Consequatur a et quam. Blanditiis iusto consequatur id quia dolorum. " +
+		"Repellat autem quae nisi earum explicabo corporis dignissimos magnam";
+
+	for(let i = 0; i < 80; i++) {
+		fetch("/webring", {
+			body: JSON.stringify({
+				name: `test_webring_${createRandomString()}`,
+				url: createRandomString(),
+				description: testDescription,
+				privateRing: false,
+				tags: []
+			}),
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+		}).then((response) => {
+			if (!response.ok) {
+				return response.json().then((errorResponse) => {
+					formErrorMessage.textContent = errorResponse.error;
+				});
+			}
+
+			console.log('OK');
+		}).catch((err) => {
+			console.error(err);
+			formErrorMessage.textContent = "An unhandled exception has occurred";
+		});
+	}
+}
