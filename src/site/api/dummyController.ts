e75cd04614c1d8a3760350a901509325b88279dc
Anthony <ajxs@panoptic.online>
2022-03-31 23:52:01 +1100

    Improve dummy controller documentation

--- src/site/api/dummyController.ts
+++ src/site/api/dummyController.ts
@@ -1,6 +1,15 @@
 
 import { NextFunction, Request, Response } from 'express';
 
+/**
+ * Dummy controller.
+ * Used as a placeholder for future implementation.
+ * @deprecated
+ * @param {Request} req Express request body.
+ * @param {Response} res Express Response.
+ * @param {NextFunction} next Express next middleware handler.
+ * @returns Renders the view.
+ */
 export function dummyController(req: Request,
 	res: Response,
 	next: NextFunction): Response
