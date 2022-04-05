1f30b40d443a9eb147786b7c18ba35bc32c91dbf
Anthony <ajxs@panoptic.online>
2022-04-05 17:32:07 +1000

    Test configuration cleanup

--- src/site/config/environment/test.ts
+++ src/site/config/environment/test.ts
@@ -6,35 +6,10 @@ import { defaultConfig } from '../defaultConfig';
  */
 export const testEnvironmentConfig: Config = {
 	...defaultConfig,
-	email: {
-		from: 'noreply@webri.ng',
-		bcc: null,
-		transport: {
-			name: 'fakeSMTP',
-			host: 'localhost',
-			port: 3725,
-			secure: false
-		}
-	},
 	global: {
 		baseDomainUrl: 'http://localhost:3456'
 	},
 	server: {
 		port: 3456,
-	},
-	user: {
-		usernameRequirements: {
-			minLength: 2,
-			maxLength: 80
-		},
-		password: {
-			minLength: 8,
-			maxLength: 128,
-			tempPasswordExpiryPeriod: [1, 'day'],
-			expiryPeriod: [3, 'months'],
-			saltRounds: 10,
-			resetTokenValidity: [1, 'hour']
-		},
-		maxUnsuccessfulLoginAttempts: 3,
 	},
 };
