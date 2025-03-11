440adb1fc79a3139cec35c5eeabae54a46279fc2
Anthony <ajxs@panoptic.online>
2025-03-12 08:40:02 +1100

    Improve email logging

--- src/site/infra/email/index.ts
+++ src/site/infra/email/index.ts
@@ -3,37 +3,22 @@ import * as nodemailer from 'nodemailer';
 import { emailConfig } from '../../config';
 import { logger } from '../../app';
 
-
 /** Type representing the data dependencies for an email attachment. */
 export type IEmailAttachment = {
 	/** The name of the attachment file. */
-	filename: string|false;
+	filename: string | false;
 	/** A Node file stream or buffer object representing the attachment file. */
-	content: string|Buffer|Readable;
-}
-
-
-/** Type representing options for the email sending process. */
-export type IEmailOptions = {
-	/** The FROM address to use in the SMTP request. */
-	from?: string;
-	/** Addresses from this value get added to RCPT TO list. */
-	bcc?: string;
-	/** The email attachments. */
-	attachments?: IEmailAttachment[];
-}
-
+	content: string | Buffer | Readable;
+};
 
 /** The email transport instance used to send email. */
 // eslint-disable-next-line
 let emailTransport: nodemailer.Transporter<any>;
 
-
 /**
  * Initialises the email transport.
  */
-function createTransport(): Promise<void>
-{
+function createTransport(): Promise<void> {
 	logger.debug('Creating email transport');
 
 	emailTransport = nodemailer.createTransport(emailConfig.transport);
@@ -51,7 +36,6 @@ function createTransport(): Promise<void>
 	});
 }
 
-
 /**
  * Sends an email via the configured email service.
  *  See: https://nodemailer.com/message/
@@ -61,23 +45,35 @@ function createTransport(): Promise<void>
  * @param {string} htmlContent - The html message content of the email.
  * @param {IEmailOptions} [options] - Additional options for the email sending process.
  */
-export async function sendEmail(to: string,
+export async function sendEmail(
+	to: string,
 	subject: string,
 	htmlContent: string,
-	options: IEmailOptions = {}): Promise<void>
-{
+	options?: Partial<{
+		/** The FROM address to use in the SMTP request. */
+		from: string;
+		/** Addresses from this value get added to RCPT TO list. */
+		bcc: string;
+		/** The email attachments. */
+		attachments: IEmailAttachment[];
+	}>
+): Promise<void> {
 	if (!emailTransport) {
 		await createTransport();
 	}
 
 	await emailTransport.sendMail({
 		to,
-		from: options.from || emailConfig.from,
-		bcc: options.bcc || emailConfig.bcc || undefined,
+		from: options?.from ?? emailConfig.from,
+		bcc: options?.bcc ?? emailConfig.bcc ?? undefined,
 		subject,
 		html: htmlContent,
-		attachments: options.attachments || []
+		attachments: options?.attachments ?? []
 	});
 
-	logger.debug(`Sent email '${subject}' to '${to}'`);
+	logger.debug('Sent email', {
+		to,
+		subject,
+		body: htmlContent
+	});
 }
