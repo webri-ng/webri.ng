23c852fbbbe275ebfb0f0ab490d5a599fee2c826
Anthony <ajxs@panoptic.online>
2026-02-24 10:07:30 +1100

    Correct spelling error in 'createResetPaswordEmailContent' module name

--- src/app/user/resetPassword/sendResetPaswordEmail/createResetPasswordEmailContent.spec.ts
+++ src/app/user/resetPassword/sendResetPaswordEmail/createResetPasswordEmailContent.spec.ts
@@ -5,35 +5,35 @@ import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import { User } from '../../../../model';
 import { userService, testUtils } from '../../..';
-import { createResetPasswordEmailContent } from './createResetPaswordEmailContent';
+import { createResetPasswordEmailContent } from './createResetPasswordEmailContent';
 
 chai.use(chaiAsPromised);
 
-describe('Create reset password email content', function ()
-{
+describe('Create reset password email content', function () {
 	this.timeout(testUtils.defaultTestTimeout);
 
 	let testUser: User;
 
-	before(async function beforeTesting()
-	{
+	before(async function beforeTesting() {
 		testUser = await testUtils.insertTestUser({
 			username: 'test_registration_email_user'
 		});
 	});
 
-
-	after(async function tearDown()
-	{
+	after(async function tearDown() {
 		await userService.deleteUser(testUser.userId!);
 	});
 
-
-	it('should create the content for a user\'s reset password email', async function ()
-	{
+	it("should create the content for a user's reset password email", async function () {
 		const temporaryPassword = 'aaaa1234';
-		const emailContent = await createResetPasswordEmailContent(testUser, temporaryPassword);
-		const contentHash = crypto.createHash('md5').update(emailContent).digest('hex');
+		const emailContent = await createResetPasswordEmailContent(
+			testUser,
+			temporaryPassword
+		);
+		const contentHash = crypto
+			.createHash('md5')
+			.update(emailContent)
+			.digest('hex');
 
 		expect(contentHash).to.equal('d35e136bcf2080f912e25a3d5743332d');
 	});
--- src/app/user/resetPassword/sendResetPaswordEmail/sendResetPaswordEmail.ts
+++ src/app/user/resetPassword/sendResetPaswordEmail/sendResetPaswordEmail.ts
@@ -1,17 +1,21 @@
 import { sendEmail } from '../../../../infra/email';
 import { User } from '../../../../model';
-import { createResetPasswordEmailContent } from './createResetPaswordEmailContent';
+import { createResetPasswordEmailContent } from './createResetPasswordEmailContent';
 
 /**
  * Sends the reset password email to a user.
  * @param {User} user The new user to send the registration email to.
  * @param {string} temporaryPassword The user's new temporary password.
  */
-export async function sendResetPaswordEmail(user: Readonly<User>,
-	temporaryPassword: string): Promise<void>
-{
+export async function sendResetPaswordEmail(
+	user: Readonly<User>,
+	temporaryPassword: string
+): Promise<void> {
 	const title = 'Your Webri.ng password has been reset';
-	const htmlContent = await createResetPasswordEmailContent(user, temporaryPassword);
+	const htmlContent = await createResetPasswordEmailContent(
+		user,
+		temporaryPassword
+	);
 
 	await sendEmail(user.email, title, htmlContent);
 }
